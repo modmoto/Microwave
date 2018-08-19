f560c5ee66ebb90000ce4f26e651fb2caff524fe
Simon Heiss <simonheiss87@gmail.com>
2018-08-19 15:42:08 +0200

    entity now save

--- Domain.Framework.Tests/EntityTests.cs
+++ Domain.Framework.Tests/EntityTests.cs
@@ -13,6 +13,51 @@ namespace Domain.Framework.Tests
             testUser.Apply(new TestUserCreatedEvent(newGuid));
             Assert.Equal(newGuid, testUser.Id);
         }
+
+        [Fact]
+        public void ApplyGenericMethod_NoApplyMethodFound()
+        {
+            var testUser = (Entity) new TestUser();
+            testUser.Apply(new TestUserNeverDidThatEvent(Guid.NewGuid()));
+            Assert.Equal(new Guid(), testUser.Id);
+        }
+
+        [Fact]
+        public void ApplyGenericMethod_ApplyMethodWithNotParameter()
+        {
+            var testUser = (Entity) new TestUserWithNoApplyMethod();
+            testUser.Apply(new TestUserCreatedEvent(Guid.NewGuid()));
+            Assert.Equal(new Guid(), testUser.Id);
+        }
+
+        [Fact]
+        public void ApplyGenericMethod_ApplyMethodWithMultipleApplyParameters()
+        {
+            var testUser = (Entity) new TestUserMultipleNoApplyMethod();
+            testUser.Apply(new TestUserCreatedEvent(Guid.NewGuid()));
+            Assert.Equal(new Guid(), testUser.Id);
+        }
+    }
+
+    internal class TestUserMultipleNoApplyMethod : Entity
+    {
+        public void Apply(TestUserCreatedEvent domainEvent, TestUserNeverDidThatEvent didThatEvent)
+        {
+        }
+    }
+
+    internal class TestUserWithNoApplyMethod : Entity
+    {
+        public void Apply()
+        {
+        }
+    }
+
+    internal class TestUserNeverDidThatEvent : DomainEvent
+    {
+        public TestUserNeverDidThatEvent(Guid newGuid) : base(newGuid)
+        {
+        }
     }
 
     internal class TestUserCreatedEvent : DomainEvent
--- Domain.Framework/Entity.cs
+++ Domain.Framework/Entity.cs
@@ -12,7 +12,8 @@ namespace Domain.Framework
             var type = domainEvent.GetType();
             var currentEntityType = GetType();
             var methodInfos = currentEntityType.GetMethods().Where(method => method.Name == nameof(Apply));
-            var methodToExecute = methodInfos.First(method => method.GetParameters().First().ParameterType == type);
+            var methodToExecute = methodInfos.FirstOrDefault(method => method.GetParameters().FirstOrDefault()?.ParameterType == type);
+            if (methodToExecute == null || methodToExecute.GetParameters().Length != 1) return;
             methodToExecute.Invoke(this, new object[] {domainEvent});
         }
     }
