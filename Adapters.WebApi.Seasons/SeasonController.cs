c8c092bc06cfc55797647caea6d16756c8c1395c
Simon Heiss <simonheiss87@gmail.com>
2018-08-19 20:15:19 +0200

    added querry interface for seasons

--- Adapters.WebApi.Seasons/SeasonController.cs
+++ Adapters.WebApi.Seasons/SeasonController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Seasons;
 using Domain.Seasons;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +10,47 @@ namespace Adapters.WebApi.Seasons
     [Route("api/seasons")]
     public class SeasonController : Controller
     {
-        [HttpGet]
-        public List<Season> GetSeasons()
+        private readonly SeasonCommandHandler _commandHandler;
+        private readonly ISeasonQuerryHandler _seasonQuerryHandler;
+
+        public SeasonController(SeasonCommandHandler commandHandler, ISeasonQuerryHandler seasonQuerryHandler)
         {
-            return new List<Season> { new Season { Id = Guid.NewGuid()}};
+            _commandHandler = commandHandler;
+            _seasonQuerryHandler = seasonQuerryHandler;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSeasons()
+        {
+            var seasons = await _seasonQuerryHandler.GetAllSeasons();
+            return Ok(seasons);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateSeason([FromBody] CreateSesonCommand command)
+        {
+            await _commandHandler.CreateSeason(command);
+            Ok();
+        }
+
+        [HttpPut("{entityId}/ChangeDate")]
+        public async Task<IActionResult> CreateSeason(Guid entityId,[FromBody] ChangeDateApiCommand command)
+        {
+            var changeDateCommand = new ChangeDateCommand
+            {
+                EntityId = entityId,
+                StartDate = command.StartDate,
+                EndDate = command.EndDate
+            };
+
+            await _commandHandler.SetStartAndEndDate(changeDateCommand);
+            Ok();
+        }
+    }
+
+    public class ChangeDateApiCommand
+    {
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
     }
 }
---
+++ Application.Seasons/ISeasonQuerryHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Seasons;
+
+namespace Application.Seasons
+{
+    public interface ISeasonQuerryHandler
+    {
+        Task<IEnumerable<Season>> GetAllSeasons();
+    }
+}
