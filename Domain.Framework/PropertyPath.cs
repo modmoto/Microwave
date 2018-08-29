a939b93eb8313e52a24ea7caf61a077ba0677c22
Simon Heiss <simonheiss87@gmail.com>
2018-08-29 12:14:19 +0200

    property path added

--- Domain.Framework/PropertyPath.cs
+++ Domain.Framework/PropertyPath.cs
@@ -1,13 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Domain.Framework
 {
     public class PropertyPath : Attribute
     {
         public string Path { get; }
+        public string PropetyName => Path.Split(".").Last();
+        public IEnumerable<string> CascadingProperties
+        {
+            get
+            {
+                var splitPath = Path.Split(".");
+                return splitPath.Take(splitPath.Length - 1);
+            }
+        }
 
         public PropertyPath(string path)
         {
+            var doesNotStartOrEndWithDots = new Regex("^[^.].*[^.]$");
+            var match = doesNotStartOrEndWithDots.Match(path);
+            if (!match.Success) throw new ArgumentException($"{nameof(path)} can not start or end with the character: \".\"");
             Path = path;
         }
     }
