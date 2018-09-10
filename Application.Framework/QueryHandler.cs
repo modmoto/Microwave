b03dd3f6d48db5086740c97fd06549fbc18faa0d
Simon Heiss <simonheiss87@gmail.com>
2018-09-10 23:56:12 +0200

    renamed

--- Application.Framework/QueryHandler.cs
+++ Application.Framework/QueryHandler.cs
@@ -6,13 +6,12 @@ namespace Application.Framework
 {
     public class QueryHandler<T> : IQueryHandler where T : Query
     {
-        public readonly SubscribedEventTypes<T> SubscribedEventTypes;
         public readonly T QueryObject;
 
         public QueryHandler(T queryObject, SubscribedEventTypes<T> subscribedEventTypes)
         {
             QueryObject = queryObject;
-            SubscribedEventTypes = subscribedEventTypes;
+            SubscribedTypes = subscribedEventTypes;
         }
 
         public void Handle(DomainEvent domainEvent, long version = 0)
@@ -21,7 +20,7 @@ namespace Application.Framework
             LastSubscriptionVersion = version;
         }
 
-        public IEnumerable<Type> SubscribedTypes => SubscribedEventTypes;
+        public IEnumerable<Type> SubscribedTypes { get; }
         public long LastSubscriptionVersion { get; private set; }
     }
 }
