035f0d83af6b117ca1ffa6dc872574edba225b56
Simon Heiss <simonheiss87@gmail.com>
2018-10-23 00:35:36 +0200

    fix update bash

--- OnlineLeagueBackend/ResetDbQuery.sh
+++ OnlineLeagueBackend/ResetDbQuery.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-rm -f OnlineLeagueBackend/Eventstore.db
+rm -f OnlineLeagueBackend/QueryContext.db
 cd Adapters.Framework.EventStores/
 
 dotnet ef migrations remove -s ../OnlineLeagueBackend/ --context QueryContext
-dotnet ef migrations add InitialMigration -s ../OnlineLeagueBackend/ --context QueryContext
+dotnet ef migrations add InitialMigrationQuery -s ../OnlineLeagueBackend/ --context QueryContext
 dotnet ef database update -s ../OnlineLeagueBackend/ --context QueryContext
 
 cd ..
