8095c08a5248fb719bdfc00e00920dad6563e4ef
Simon Heiss <simonheiss87@gmail.com>
2018-12-11 07:33:38 +0100

    added a test for peace of mind

---
+++ Microwave.Application.UnitTests/ResultTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microwave.Application.Exceptions;
+using Microwave.Application.Results;
+
+namespace Microwave.Application.UnitTests
+{
+    [TestClass]
+    public class ResultTests
+    {
+        [TestMethod]
+        public void Result_DoesNotThrowExceptionWithPrimitiveTypes()
+        {
+            var notFound = Result<long>.NotFound("123");
+            Assert.ThrowsException<NotFoundException>(() => notFound.Value);
+        }
+    }
+}
