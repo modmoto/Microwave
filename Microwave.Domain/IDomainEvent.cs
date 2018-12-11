75298e80ada493980989552cae1db527f2bcb2a3
Simon Heiss <simonheiss87@gmail.com>
2018-12-11 23:30:33 +0100

    Revert "added new identity thing"
    This reverts commit 83c8706

--- Microwave.Domain/IDomainEvent.cs
+++ Microwave.Domain/IDomainEvent.cs
@@ -6,32 +6,4 @@ namespace Microwave.Domain
     {
         Guid EntityId { get; }
     }
-
-    public interface IIdentifiable
-    {
-        bool IsSameId(object other);
-    }
-
-    public class Identifier<T> : IIdentifiable
-    {
-        public T Id { get; }
-
-        public Identifier(T id)
-        {
-            Id = id;
-        }
-
-        public bool IsSameId(object other)
-        {
-            var type = other?.GetType();
-            var propertyInfo = type?.GetProperty("Id");
-            var value = propertyInfo?.GetValue(other);
-            return Id.Equals(value);
-        }
-    }
-
-    public interface IIdentifiableDomainEvent
-    {
-        IIdentifiable EntityId { get; }
-    }
 }
