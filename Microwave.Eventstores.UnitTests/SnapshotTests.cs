21dc7b1c9efd96e0c0773822a6062f21ec144243
Simon Heiss <simonheiss87@gmail.com>
2018-12-13 23:17:33 +0100

    removed unused usings

--- Microwave.Eventstores.UnitTests/SnapshotTests.cs
+++ Microwave.Eventstores.UnitTests/SnapshotTests.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microwave.Domain;
 using Microwave.EventStores;
