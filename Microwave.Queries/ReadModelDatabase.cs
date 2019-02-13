36265b570a39e9570b35d546f44571db2849ba8d
Simon Heiss <simonheiss87@gmail.com>
2019-02-13 19:43:19 +0100

    fixed warning for good

--- Microwave.Queries/ReadModelDatabase.cs
+++ Microwave.Queries/ReadModelDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using MongoDB.Driver;
 
 namespace Microwave.Queries
@@ -42,6 +43,14 @@ namespace Microwave.Queries
     [Serializable]
     public class ReadModelConfig : Dictionary<Type, Uri>
     {
+        public ReadModelConfig()
+        {
+        }
+
+        protected ReadModelConfig(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
         public Uri TryGet(Type type)
         {
             return ContainsKey(type) ? this[type] : null;
@@ -51,6 +60,14 @@ namespace Microwave.Queries
     [Serializable]
     public class DomainEventConfig : Dictionary<Type, Uri>
     {
+        public DomainEventConfig()
+        {
+        }
+
+        protected DomainEventConfig(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
         public Uri TryGet(Type type)
         {
             return ContainsKey(type) ? this[type] : null;
