9bd5a689765acdb7b7447833d65f4ea077ee7aef
Simon Heiss <simonheiss87@gmail.com>
2019-03-30 18:35:42 +0100

    fixed tests

--- Microwave.WebApi.UnitTests/DiscoveryRepositoryTests.cs
+++ Microwave.WebApi.UnitTests/DiscoveryRepositoryTests.cs
@@ -14,15 +14,16 @@ namespace Microwave.WebApi.UnitTests
         public async Task GetPublishedEvents()
         {
             var mockHttp = new MockHttpMessageHandler();
-            mockHttp.When("http://localost:5000/Dicovery/PublishedEvents")
+            mockHttp.When("http://localhost:5000/Dicovery/PublishedEvents")
                 .Respond("application/json", "[ \"event1\", \"event2\"]");
 
             var client = new DiscoveryClient(mockHttp);
+            client.BaseAddress = new Uri("http://localhost:5000/");
 
             var mock = new Mock<IDiscoveryClientFactory>();
             mock.Setup(m => m.GetClient(It.IsAny<Uri>())).Returns(client);
             var serviceDiscoveryRepository = new ServiceDiscoveryRepository(mock.Object);
-            var serviceAdress = new Uri("http://localost:5000/");
+            var serviceAdress = new Uri("http://localhost:5000/");
             var publishedEventTypes = serviceDiscoveryRepository.GetPublishedEventTypes(serviceAdress);
 
             var publisherEventConfig = publishedEventTypes.Result;
