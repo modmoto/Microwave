d3f3c0fcb79edbf08ff813a560d54cffd00088a3
Simon Heiss <simonheiss87@gmail.com>
2019-04-01 22:33:56 +0200

    added layout to see if this works as nuget

---
+++ Microwave/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microwave.Application.Discovery;
+
+namespace Teams.ReadHost.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DiscoveryHandler _discoveryHandler;
+
+        public IEventLocation ConsumingServices { get; set; }
+
+        public IndexModel(DiscoveryHandler discoveryHandler)
+        {
+            _discoveryHandler = discoveryHandler;
+        }
+
+        public void OnGet()
+        {
+            var consumingServices = _discoveryHandler.GetConsumingServices();
+            ConsumingServices = consumingServices;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _discoveryHandler.DiscoverConsumingServices();
+            return Redirect("#");
+        }
+    }
+}
