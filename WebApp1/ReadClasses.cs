e5ab6c36af492fdd201a402184ba8efc7c460356
Simon Heiss <simonheiss87@gmail.com>
2019-06-12 06:31:15 +0200

    ui working this way

---
+++ WebApp1/ReadClasses.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Threading.Tasks;
+using Microwave.Domain.Identities;
+using Microwave.Queries;
+
+namespace WebApp1
+{
+    public class Handler1 : IHandleAsync<Event2>, IHandleAsync<Event4>, IHandleAsync<EventNotPublished>
+    {
+        public Task HandleAsync(Event2 domainEvent)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task HandleAsync(Event4 domainEvent)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task HandleAsync(EventNotPublished domainEvent)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class EventNotPublished : ISubscribedDomainEvent
+    {
+        public EventNotPublished(Identity entityId)
+        {
+            EntityId = entityId;
+        }
+
+        public Identity EntityId { get; }
+    }
+
+    public class ReadModel1 : ReadModel, IHandle<Event3>, IHandle<Event4>
+    {
+        public override Type GetsCreatedOn => typeof(Event3);
+
+        public void Handle(Event3 domainEvent)
+        {
+        }
+
+        public void Handle(Event4 domainEvent)
+        {
+        }
+    }
+
+    public class ReadModelNotPublished : ReadModel, IHandle<EventNotPublished>
+    {
+        public override Type GetsCreatedOn => typeof(EventNotPublished);
+
+        public void Handle(EventNotPublished domainEvent)
+        {
+        }
+    }
+
+    public class Event1 : ISubscribedDomainEvent
+    {
+        public Event1(Identity entityId)
+        {
+            EntityId = entityId;
+        }
+
+        public Identity EntityId { get; }
+    }
+
+
+    public class Event2 : ISubscribedDomainEvent
+    {
+        public Event2(Identity entityId)
+        {
+            EntityId = entityId;
+        }
+
+
+        public string Name { get; }
+        public string SurName { get; }
+
+        public Identity EntityId { get; }
+    }
+
+    public class Event3 : ISubscribedDomainEvent
+    {
+        public Event3(Identity entityId)
+        {
+            EntityId = entityId;
+        }
+
+        public string RmName { get; }
+        public string RmSurName { get; }
+        public Identity EntityId { get; }
+    }
+
+
+    public class Event4 : ISubscribedDomainEvent
+    {
+        public Event4(Identity entityId)
+        {
+            EntityId = entityId;
+        }
+
+        public int Age { get; }
+        public string Name { get; }
+        public string WeirdName { get; }
+
+        public Identity EntityId { get; }
+    }
+}
