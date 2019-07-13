edec86d7aff15bd3ca18f7c29658f08463da36b7
Simon Heiss <simonheiss87@gmail.com>
2019-07-13 14:12:39 +0200

    added auth to other services

--- TestServices/WriteService1/Startup.cs
+++ TestServices/WriteService1/Startup.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using Microwave;
 using Microwave.Persistence.MongoDb;
 using Microwave.UI;
+using ReadService1;
 using ServerConfig;
 
 namespace WriteService1
@@ -19,7 +22,13 @@ namespace WriteService1
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .AddRequirements(new ApiKeyRequirement())
+                    .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddMicrowaveUi();
 
             services.AddMicrowave(_microwaveConfiguration, new MongoDbPersistenceLayer(new MicrowaveMongoDb { DatabaseName = "TestWriteService1ReadDb"}));
