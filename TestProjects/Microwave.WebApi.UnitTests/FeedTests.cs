f5a3262d71f58adb5b4885a7df0cd5130d41bc92
Simon Heiss <simonheiss87@gmail.com>
2019-07-13 16:27:58 +0200

    added back commented out tests

--- TestProjects/Microwave.WebApi.UnitTests/FeedTests.cs
+++ TestProjects/Microwave.WebApi.UnitTests/FeedTests.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microwave.Domain.EventSourcing;
 using Microwave.Domain.Identities;
 using Microwave.Queries;
 using Microwave.WebApi.Querries;
+using Moq;
+using RichardSzalay.MockHttp;
 
 namespace Microwave.WebApi.UnitTests
 {
@@ -15,48 +20,48 @@ namespace Microwave.WebApi.UnitTests
             { nameof(TestEv), typeof(TestEv)}
         };
 
-//        [TestMethod]
-//        public async Task ReadModelFeed()
-//        {
-//            var mockHttp = new MockHttpMessageHandler();
-//            mockHttp.When("http://localost:5000/api/DomainEvents/?timeStamp=0001-01-01T00:00:00.0000000+00:00")
-//                .Respond("application/json", "[{ \"domainEventType\": \"UNKNOWN_TYPE\",\"version\": 12, \"created\": \"2018-01-07T19:07:21.227631+01:00\", \"domainEvent\": {\"EntityId\" : \"5a8b63c8-0f7f-4de7-a9e5-b6b377aa2180\"}}, { \"domainEventType\": \"TestEv\",\"version\": 12, \"created\": \"2018-01-07T19:07:31.227631+01:00\", \"domainEvent\": {\"EntityId\" : \"5a8b63c8-0f7f-4de7-a9e5-b6b377aa2180\" }}]");
-//
-//            var domainOverallEventClient = new DomainEventClient<ReadModelHandler<TestReadModel>>(mockHttp);
-//            domainOverallEventClient.BaseAddress = new Uri("http://localost:5000/api/DomainEvents/");
-//
-//            var factoryMock = new Mock<IDomainEventClientFactory>();
-//            factoryMock.Setup(m => m.GetClient<ReadModelHandler<TestReadModel>>()).ReturnsAsync(domainOverallEventClient);
-//            var domainEventFactory = new DomainEventFactory(_eventTypeRegistration);
-//            var readModelFeed = new EventFeed<ReadModelHandler<TestReadModel>>(
-//                domainEventFactory,
-//                factoryMock.Object);
-//            var domainEvents = await readModelFeed.GetEventsAsync();
-//            var domainEventWrappers = domainEvents.ToList();
-//            Assert.AreEqual(1, domainEventWrappers.Count);
-//            Assert.AreEqual(new Guid("5a8b63c8-0f7f-4de7-a9e5-b6b377aa2180").ToString(), domainEventWrappers[0].DomainEvent
-//                .EntityId.Id);
-//        }
-//
-//        [TestMethod]
-//        public async Task ReadModelFeed_Exception()
-//        {
-//            var mockHttp = new MockHttpMessageHandler();
-//            mockHttp.When("http://localost:5000/api/DomainEvents/?timeStamp=0001-01-01T00:00:00.0000000+00:00")
-//                .Throw(new HttpRequestException());
-//            var domainOverallEventClient = new DomainEventClient<ReadModelHandler<TestReadModel>>(mockHttp);
-//            domainOverallEventClient.BaseAddress = new Uri("http://localost:5000/api/DomainEvents/");
-//
-//            var factoryMock = new Mock<IDomainEventClientFactory>();
-//            factoryMock.Setup(m => m.GetClient<ReadModelHandler<TestReadModel>>()).ReturnsAsync(domainOverallEventClient);
-//
-//            var domainEventFactory = new DomainEventFactory(_eventTypeRegistration);
-//            var readModelFeed = new EventFeed<ReadModelHandler<TestReadModel>>(domainEventFactory, factoryMock.Object);
-//            var domainEvents = await readModelFeed.GetEventsAsync();
-//            var domainEventWrappers = domainEvents.ToList();
-//
-//            Assert.AreEqual(0, domainEventWrappers.Count);
-//        }
+        [TestMethod]
+        public async Task ReadModelFeed()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When("http://localost:5000/api/DomainEvents/?timeStamp=0001-01-01T00:00:00.0000000+00:00")
+                .Respond("application/json", "[{ \"domainEventType\": \"UNKNOWN_TYPE\",\"version\": 12, \"created\": \"2018-01-07T19:07:21.227631+01:00\", \"domainEvent\": {\"EntityId\" : \"5a8b63c8-0f7f-4de7-a9e5-b6b377aa2180\"}}, { \"domainEventType\": \"TestEv\",\"version\": 12, \"created\": \"2018-01-07T19:07:31.227631+01:00\", \"domainEvent\": {\"EntityId\" : \"5a8b63c8-0f7f-4de7-a9e5-b6b377aa2180\" }}]");
+
+            var domainOverallEventClient = new HttpClient(mockHttp);
+            domainOverallEventClient.BaseAddress = new Uri("http://localost:5000/api/DomainEvents/");
+
+            var factoryMock = new Mock<IDomainEventClientFactory>();
+            factoryMock.Setup(m => m.GetClient<ReadModelHandler<TestReadModel>>()).ReturnsAsync(domainOverallEventClient);
+            var domainEventFactory = new DomainEventFactory(_eventTypeRegistration);
+            var readModelFeed = new EventFeed<ReadModelHandler<TestReadModel>>(
+                domainEventFactory,
+                factoryMock.Object);
+            var domainEvents = await readModelFeed.GetEventsAsync();
+            var domainEventWrappers = domainEvents.ToList();
+            Assert.AreEqual(1, domainEventWrappers.Count);
+            Assert.AreEqual(new Guid("5a8b63c8-0f7f-4de7-a9e5-b6b377aa2180").ToString(), domainEventWrappers[0].DomainEvent
+                .EntityId.Id);
+        }
+
+        [TestMethod]
+        public async Task ReadModelFeed_Exception()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When("http://localost:5000/api/DomainEvents/?timeStamp=0001-01-01T00:00:00.0000000+00:00")
+                .Throw(new HttpRequestException());
+            var domainOverallEventClient = new HttpClient(mockHttp);
+            domainOverallEventClient.BaseAddress = new Uri("http://localost:5000/api/DomainEvents/");
+
+            var factoryMock = new Mock<IDomainEventClientFactory>();
+            factoryMock.Setup(m => m.GetClient<ReadModelHandler<TestReadModel>>()).ReturnsAsync(domainOverallEventClient);
+
+            var domainEventFactory = new DomainEventFactory(_eventTypeRegistration);
+            var readModelFeed = new EventFeed<ReadModelHandler<TestReadModel>>(domainEventFactory, factoryMock.Object);
+            var domainEvents = await readModelFeed.GetEventsAsync();
+            var domainEventWrappers = domainEvents.ToList();
+
+            Assert.AreEqual(0, domainEventWrappers.Count);
+        }
     }
 
     public class TestReadModel : ReadModel
