517044e84dcaab29c55364ba599f8077df6c5c38
Simon Heiss <simonheiss87@gmail.com>
2019-08-07 23:30:46 +0200

    rename stuff

--- Microwave.Persistence.InMemory/Querries/ReadModelRepositoryInMemory.cs
+++ Microwave.Persistence.InMemory/Querries/ReadModelRepositoryInMemory.cs
@@ -10,11 +10,11 @@ namespace Microwave.Persistence.InMemory.Querries
 {
     public class ReadModelRepositoryInMemory : IReadModelRepository
     {
-        private Dictionary<Type, object> querryDictionary = new Dictionary<Type, object>();
-        private Dictionary<Identity, object> readModelDictionary = new Dictionary<Identity, object>();
+        private readonly Dictionary<Type, object> _querryDictionary = new Dictionary<Type, object>();
+        private readonly Dictionary<Identity, object> _readModelDictionary = new Dictionary<Identity, object>();
         public Task<Result<T>> Load<T>() where T : Query
         {
-            if (!querryDictionary.TryGetValue(typeof(T), out var query)) return Task.FromResult(Result<T>.NotFound
+            if (!_querryDictionary.TryGetValue(typeof(T), out var query)) return Task.FromResult(Result<T>.NotFound
             (StringIdentity.Create(nameof(T))));
             var queryParsed = query as T;
             return Task.FromResult(Result<T>.Ok(queryParsed));
@@ -22,7 +22,7 @@ namespace Microwave.Persistence.InMemory.Querries
 
         public Task<ReadModelResult<T>> Load<T>(Identity id) where T : ReadModel
         {
-            if (id == null || !readModelDictionary.TryGetValue(id, out var readModel)) return Task.FromResult
+            if (id == null || !_readModelDictionary.TryGetValue(id, out var readModel)) return Task.FromResult
             (ReadModelResult<T>.NotFound(id));
             var model = readModel as T;
             if (model == null) return Task.FromResult(ReadModelResult<T>.NotFound(id));
@@ -32,19 +32,19 @@ namespace Microwave.Persistence.InMemory.Querries
 
         public Task<Result> Save<T>(T query) where T : Query
         {
-            querryDictionary[typeof(T)] = query;
+            _querryDictionary[typeof(T)] = query;
             return Task.FromResult(Result.Ok());
         }
 
         public Task<Result> Save<T>(T readModel, Identity identity, long version) where T : ReadModel, new()
         {
-            readModelDictionary[identity] = readModel;
+            _readModelDictionary[identity] = readModel;
             return Task.FromResult(Result.Ok());
         }
 
         public Task<Result<IEnumerable<T>>> LoadAll<T>() where T : ReadModel
         {
-            var loadAll = readModelDictionary.Values.Where(v => v.GetType() == typeof(T)).ToList();
+            var loadAll = _readModelDictionary.Values.Where(v => v.GetType() == typeof(T)).ToList();
             if (loadAll.Count == 0) return Task.FromResult(Result<IEnumerable<T>>.NotFound(StringIdentity.Create(nameof(T))));
             return Task.FromResult(Result<IEnumerable<T>>.Ok(loadAll.Select(l => l as T)));
         }
