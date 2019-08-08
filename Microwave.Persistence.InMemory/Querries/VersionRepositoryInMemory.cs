914fcccaf1559504f87ef89e78d9dbe495974372
Simon Heiss <simonheiss87@gmail.com>
2019-08-08 23:36:57 +0200

    rename, remove warning

--- Microwave.Persistence.InMemory/Querries/VersionRepositoryInMemory.cs
+++ Microwave.Persistence.InMemory/Querries/VersionRepositoryInMemory.cs
@@ -8,18 +8,18 @@ namespace Microwave.Persistence.InMemory.Querries
 {
     public class VersionRepositoryInMemory : IVersionRepository
     {
-        private ConcurrentDictionary<string, DateTimeOffset> versionDictionray = new ConcurrentDictionary<string, DateTimeOffset>();
+        private readonly ConcurrentDictionary<string, DateTimeOffset> _versionDictionray = new ConcurrentDictionary<string, DateTimeOffset>();
         public Task<DateTimeOffset> GetVersionAsync(string domainEventType)
         {
             if (domainEventType == null) return Task.FromResult(DateTimeOffset.MinValue);
-            if (!versionDictionray.TryGetValue(domainEventType, out var version)) return Task.FromResult(DateTimeOffset.MinValue);
+            if (!_versionDictionray.TryGetValue(domainEventType, out var version)) return Task.FromResult(DateTimeOffset.MinValue);
 
             return Task.FromResult(version);
         }
 
         public Task SaveVersion(LastProcessedVersion version)
         {
-            versionDictionray[version.EventType] = version.LastVersion;
+            _versionDictionray[version.EventType] = version.LastVersion;
             return Task.CompletedTask;
         }
     }
