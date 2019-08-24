747a761c5e154a361f5b4e431517569b8f87803c
Simon Heiss <simonheiss87@gmail.com>
2019-08-24 17:46:11 +0200

    removed ip function again, as url is needed

--- Microwave.WebApi/Discovery/DiscoveryController.cs
+++ Microwave.WebApi/Discovery/DiscoveryController.cs
@@ -69,70 +69,8 @@ namespace Microwave.WebApi.Discovery
         [HttpPut("SubscribeEvent")]
         public async Task<ActionResult> SubscribeEvent()
         {
-            var requestIp = GetRequestIP();
             await _discoveryHandler.SubscribeForEvent(null, null);
             return Ok();
-        }
-
-        public string GetRequestIP(bool tryUseXForwardHeader = true)
-        {
-            string ip = null;
-
-            // todo support new "Forwarded" header (2014) https://en.wikipedia.org/wiki/X-Forwarded-For
-
-            // X-Forwarded-For (csv list):  Using the First entry in the list seems to work
-            // for 99% of cases however it has been suggested that a better (although tedious)
-            // approach might be to read each IP from right to left and use the first public IP.
-            // http://stackoverflow.com/a/43554000/538763
-            //
-            if (tryUseXForwardHeader)
-                ip = SplitCsv(GetHeaderValueAs<string>("X-Forwarded-For")).FirstOrDefault();
-
-            // RemoteIpAddress is always null in DNX RC1 Update1 (bug).
-            if (IsNullOrWhitespace(ip) && _httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress != null)
-                ip = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
-
-            if (IsNullOrWhitespace(ip))
-                ip = GetHeaderValueAs<string>("REMOTE_ADDR");
-
-            // _httpContextAccessor.HttpContext?.Request?.Host this is the local host.
-
-            if (IsNullOrWhitespace(ip))
-                throw new Exception("Unable to determine caller's IP.");
-
-            return ip;
-        }
-
-        public T GetHeaderValueAs<T>(string headerName)
-        {
-            StringValues values;
-
-            if (_httpContextAccessor.HttpContext?.Request?.Headers?.TryGetValue(headerName, out values) ?? false)
-            {
-                string rawValues = values.ToString();   // writes out as Csv when there are multiple.
-
-                if (!IsNullOrWhitespace(rawValues))
-                    return (T)Convert.ChangeType(values.ToString(), typeof(T));
-            }
-            return default(T);
-        }
-
-        public static List<string> SplitCsv(string csvList, bool nullOrWhitespaceInputReturnsNull = false)
-        {
-            if (string.IsNullOrWhiteSpace(csvList))
-                return nullOrWhitespaceInputReturnsNull ? null : new List<string>();
-
-            return csvList
-                .TrimEnd(',')
-                .Split(',')
-                .AsEnumerable()
-                .Select(s => s.Trim())
-                .ToList();
-        }
-
-        public static bool IsNullOrWhitespace(string s)
-        {
-            return String.IsNullOrWhiteSpace(s);
         }
     }
 }
