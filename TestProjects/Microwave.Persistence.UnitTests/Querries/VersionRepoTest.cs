5a323de0d00e041174dcf038249104fb25525365
Simon Heiss <simonheiss87@gmail.com>
2019-08-25 23:04:14 +0200

    added tests for remote version repos

--- TestProjects/Microwave.Persistence.UnitTests/Querries/VersionRepoTest.cs
+++ TestProjects/Microwave.Persistence.UnitTests/Querries/VersionRepoTest.cs
@@ -31,5 +31,28 @@ namespace Microwave.Persistence.UnitTests.Querries
             var count = await versionRepository.GetVersionAsync(null);
             Assert.AreEqual(DateTimeOffset.MinValue, count);
         }
+
+        [DataTestMethod]
+        [PersistenceTypeTest]
+        public async Task VersionRepoRemoteVersion_DuplicateUpdate(PersistenceLayerProvider layerProvider)
+        {
+            var versionRepository = layerProvider.VersionRepository;
+
+            var dateTimeOffset = DateTimeOffset.Now;
+            await versionRepository.SaveRemoteVersionAsync(new LastProcessedVersion("Type", dateTimeOffset));
+            await versionRepository.SaveRemoteVersionAsync(new LastProcessedVersion("Type", dateTimeOffset));
+
+            var count = await versionRepository.GetVersionAsync("Type");
+            Assert.AreEqual(dateTimeOffset, count);
+        }
+
+        [DataTestMethod]
+        [PersistenceTypeTest]
+        public async Task VersionRepoRemoteVersion_LoadWithNull(PersistenceLayerProvider layerProvider)
+        {
+            var versionRepository = layerProvider.VersionRepository;
+            var count = await versionRepository.GetRemoteVersionAsync(null);
+            Assert.AreEqual(DateTimeOffset.MinValue, count);
+        }
     }
 }
