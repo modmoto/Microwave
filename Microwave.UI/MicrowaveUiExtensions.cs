68064b24a3c6a2aa0efedf7aea35408b0db5a091
Simon Heiss <simonheiss87@gmail.com>
2019-12-12 08:23:11 +0100

    removed from coverageg3

--- Microwave.UI/MicrowaveUiExtensions.cs
+++ Microwave.UI/MicrowaveUiExtensions.cs
@@ -1,33 +1,26 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microwave.UI
 {
+    [ExcludeFromCodeCoverage]
     public static class MicrowaveUiExtensions
     {
-        [ExcludeFromCodeCoverage]
         public static void AddMicrowaveUi(this IServiceCollection services)
         {
             services.ConfigureOptions(typeof(MicrowaveUiConfigureOptions));
         }
 
-        [ExcludeFromCodeCoverage]
         public static IApplicationBuilder UseMicrowaveUi(this IApplicationBuilder builder)
         {
             builder.UseRouting();
-            builder.UseEndpoints(endpoints => AddEnpoints(endpoints));
+            builder.UseEndpoints(endpoints => {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
             builder.UseStaticFiles();
             return builder;
-        }
-
-        [ExcludeFromCodeCoverage]
-        private static IEndpointRouteBuilder AddEnpoints(IEndpointRouteBuilder endpoints)
-        {
-            endpoints.MapRazorPages();
-            endpoints.MapControllers();
-            return endpoints;
         }
     }
 }
