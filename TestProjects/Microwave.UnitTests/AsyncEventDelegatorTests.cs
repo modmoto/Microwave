575bededea76d3e83f0ca87e658e1d03053a41b8
Simon Heiss <simonheiss87@gmail.com>
2020-01-23 00:01:15 +0100

    added test coverage

--- TestProjects/Microwave.UnitTests/AsyncEventDelegatorTests.cs
+++ TestProjects/Microwave.UnitTests/AsyncEventDelegatorTests.cs
@@ -10,11 +10,36 @@ namespace Microwave.UnitTests
     [TestClass]
     public class AsyncEventDelegatorTests
     {
-        [TestMethod]
-        public void RunMockWithoutAttribute()
+        [TestMethod] public void RunMockWithoutAttribute_AsyncHandler()
         {
             var asyncEventDelegator = new AsyncEventDelegator(new List<IAsyncEventHandler> {new AsyncHandlerMock()}, new
                 List<IQueryEventHandler>(), new List<IReadModelEventHandler>(), null, new List<IPollingInterval>
+            {
+                new PollingInterval<AsyncHandlerImplemented>(3)
+            });
+
+            asyncEventDelegator.StartEventPolling();
+        }
+
+        [TestMethod]
+        public void RunMockWithoutAttribute_ReadModelHandler()
+        {
+            var asyncEventDelegator = new AsyncEventDelegator(new List<IAsyncEventHandler>(), new
+                List<IQueryEventHandler>(), new List<IReadModelEventHandler> { new RmHandlerMock() }, null, new
+                List<IPollingInterval>
+            {
+                new PollingInterval<AsyncHandlerImplemented>(3)
+            });
+
+            asyncEventDelegator.StartEventPolling();
+        }
+
+        [TestMethod]
+        public void RunMockWithoutAttribute_QueryHandler()
+        {
+            var asyncEventDelegator = new AsyncEventDelegator(new List<IAsyncEventHandler>(), new
+                List<IQueryEventHandler> { new QHandlerMock() }, new List<IReadModelEventHandler>(), null, new
+                List<IPollingInterval>
             {
                 new PollingInterval<AsyncHandlerImplemented>(3)
             });
@@ -33,6 +58,22 @@ namespace Microwave.UnitTests
                 List<IQueryEventHandler>(), new List<IReadModelEventHandler>());
 
             asyncEventDelegator.StartEventPolling();
+        }
+    }
+
+    public class RmHandlerMock : IReadModelEventHandler
+    {
+        public Task Update()
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class QHandlerMock : IQueryEventHandler
+    {
+        public Task Update()
+        {
+            return Task.CompletedTask;
         }
     }
 
