b26ca72ba3bec0be3f4d7c538960cb5551661a30
Simon Heiss <simonheiss87@gmail.com>
2020-02-25 23:48:09 +0100

    job page has own lists now

--- TestProjects/Microwave.UI.UnitTests/JobPagesTests.cs
+++ TestProjects/Microwave.UI.UnitTests/JobPagesTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microwave.Queries;
 using Microwave.Queries.Handler;
 using Microwave.Queries.Polling;
 using Microwave.UI.Areas.MicrowaveDashboard.Pages;
@@ -44,6 +45,24 @@ namespace Microwave.UI.UnitTests
         }
 
         [TestMethod]
+        public void JobsPage_HandlerLists()
+        {
+            var mock1 = new MicrowaveBackgroundService<ReadModelEventHandler<TestIdQuery>>(null,
+                new PollingInterval<ReadModelEventHandler<TestIdQuery>>());
+            var mock3 = new MicrowaveBackgroundService<QueryEventHandler<QueryTest, TestDomainEvent1>>(null,
+                new PollingInterval<QueryEventHandler<QueryTest, TestDomainEvent1>>());
+            var mock2 = new MicrowaveBackgroundService<AsyncEventHandler<TestEventHandler, TestDomainEvent1>>(null,
+                new PollingInterval<AsyncEventHandler<TestEventHandler, TestDomainEvent1>>());
+            var jobsPage = new JobsPage(
+                new MicrowaveWebApiConfiguration(),
+                new List<IMicrowaveBackgroundService> { mock1, mock2, mock3});
+
+            Assert.AreEqual(1, jobsPage.AsyncHandlers.Count());
+            Assert.AreEqual(1, jobsPage.QueryHandlers.Count());
+            Assert.AreEqual(1, jobsPage.ReadModelHandlers.Count());
+        }
+
+        [TestMethod]
         public void JobsPage_DiscoveryPollerIsFilteredOut()
         {
             var mock1 = new MicrowaveBackgroundService<AsyncEventHandler<TestEventHandler, TestDomainEvent1>>(null,
@@ -55,5 +74,9 @@ namespace Microwave.UI.UnitTests
 
             Assert.AreEqual(1, jobsPage.Jobs.Count());
         }
+    }
+
+    public class QueryTest : Query
+    {
     }
 }
